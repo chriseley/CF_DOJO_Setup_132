54864b7c345f3cd6c0780bd253833d6db8b1a419
chriseley <crobineley@gmail.com>
2022-03-02 21:14:42 -0500

    Kata_06-07A

---
+++ Kata_06-07A/Program.cs
@@ -0,0 +1,33 @@
+﻿//Ask the user "What is your age"
+//Store the user input in a variable 'age'
+
+//Use if statements to determine if the userInput is 'of age' to drink
+//if age < 5 && age >=0 -> print "I'm calling your parents!"
+//else if age < 10 -> print "Come on? are you serious?"
+//else if age < 18 -> print "Sorry you are too young to drink, come back in three years!"
+//else if if age >= 21 -> print "Hawdy, What can I get you to drink!"
+//else -> print "Invalid Option, please try again."
+
+//use a terinary operator to check if a persons age using the age variable above 
+//to check if the person is a child or not   
+//so if a persons age is greater than 12 the he/she is not a child, else 
+//the person is a child 
+
+//Create an 'Array of int' that can hold a total of 10 items
+//Create a 'decimal' Array that can hold 5 names
+//Create a 'string' Array that can hold 5 names
+//Create a 'bool' Array that can hold 5 names
+
+
+//Create a 'List<int>' childAges ,that can hold a total of 5 times
+//Create a 'List<decimal>' grades ,that can hold a total of 5 times
+//Create a 'List<string>' teacherNames ,Array that can hold 5 names
+
+
+//while loop -> while loop through the variable childAges, grades, and teacherNames
+
+//for loop -> for loop through the variable childAges, grades, and teacherNames
+
+//foreach loop -> foreach through the variable childAges, grades, and teacherNames
+
+//do while loop -> do while loop through the variable childAges, grades, and teacherNames
